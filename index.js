f1268b8f9bf3e01746bcb31248e199a78a6c663b
pablo agustin <pablo_agustin87@hotmail.com>
2023-02-22 22:59:16 -0300

    actualizados y corregido la carpeta de proyecto1_cvitae 22/02/2023

---
+++ index.js
@@ -0,0 +1,33 @@
+function MostrarCorreo() {
+    document.getElementById('correo').style.display = 'block';
+}
+function OcultarCorreo() {
+    document.getElementById('correo').style.display = 'none';
+}
+
+
+
+function MostrarMovil() {
+    document.getElementById('movil').style.display = 'block';
+}
+function OcultarMovil() {
+    document.getElementById('movil').style.display = 'none';
+}
+
+
+
+function MostrarMaletin() {
+    document.getElementById('maletin').style.display = 'block';
+}
+function OcultarMaletin() {
+    document.getElementById('maletin').style.display = 'none';
+}
+
+
+function MostrarGato() {
+    document.getElementById('gato').style.display = 'block';
+}
+function OcultarGato() {
+    document.getElementById('gato').style.display = 'none';
+}
+
